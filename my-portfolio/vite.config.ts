fdf7f4e67472e30083144042a194e0e2b2e87593
Saxena15 <akash15saxena@gmail.com>
2025-08-02 15:08:52 +0530

    changes

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/My-Portfolio/', // 👈 This is necessary for GitHub Pages
   plugins: [react()],
 })
-
